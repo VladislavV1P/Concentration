9d7bd75b0efc52b258bb73906df27d7c6a622310
VladislavV1P <98524936+VladislavV1P@users.noreply.github.com>
2022-02-04 22:18:42 +0300

    button

---
+++ Concentration/Concentration/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Владислав Патраков on 04.02.2022.
+//
+
+import UIKit
+ 
+class ViewController: UIViewController {
+
+    @IBOutlet var buttonCollection: [UIButton]!
+    
+    @IBOutlet weak var touchLabel: UILabel!
+    
+    @IBAction func buttonAction(_ sender: UIButton) {
+    }
+    
+
+}
+
