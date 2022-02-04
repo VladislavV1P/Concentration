bb930fc100ce37ca58fda249d1ea7fefca5eae0e
VladislavV1P <98524936+VladislavV1P@users.noreply.github.com>
2022-02-04 23:55:30 +0300

    emoji

--- Concentration/Concentration/ViewController.swift
+++ Concentration/Concentration/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
  
 class ViewController: UIViewController {
 
+    var touches = 0 {
+        didSet {
+            touchLabel.text = "Touches: \(touches)"
+        }
+    }
+    
+    func flipButton(emoji: String, button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: .normal)
+            button.backgroundColor = #colorLiteral(red: 0.8422401547, green: 0.5098901391, blue: 1, alpha: 1)
+        } else {
+            
+            button.setTitle(emoji, for: .normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+    }
+    
+    let emojiCollection = ["🐊","🐋","🐊","🐋"]
+    
     @IBOutlet var buttonCollection: [UIButton]!
-    
     @IBOutlet weak var touchLabel: UILabel!
-    
     @IBAction func buttonAction(_ sender: UIButton) {
+        touches += 1
+        flipButton(emoji: emojiCollection[0], button:  sender)
     }
     
 
